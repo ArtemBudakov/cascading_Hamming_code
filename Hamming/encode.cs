e828da8557dbc28bb4bf1702329f17d7cbd1c43f
Unknown <Kizary-archer@yandex.ru>
2019-06-06 17:59:20 +0300

    start

---
+++ Hamming/encode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamming
+{
+    class encode
+    {
+        public encode(string mes)
+        {
+            List<int> mes = new List<int>();
+        }
+    }
+}
