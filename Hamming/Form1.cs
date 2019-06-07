df0ad876ef56d3f7a188f56e83769134530f0c29
Unknown <Kizary-archer@yandex.ru>
2019-06-07 20:35:19 +0300

    bit4

--- Hamming/Form1.cs
+++ Hamming/Form1.cs
@@ -25,10 +25,8 @@ namespace Hamming
 
         private void button1_Click(object sender, EventArgs e)
         {
-            encode enc = new encode();
-            //conversion hello = new conversion(textBox1.Text);
-            this.conv = new conversion(textBox1.Text);
-            this.conv.Transfer();
+           // encode enc = new encode();
+            conversion hello = new conversion(textBox1.Text);
         }
     }
 }
--- Hamming/conversion.cs
+++ Hamming/conversion.cs
@@ -13,53 +13,38 @@ namespace Hamming
 
     public class conversion
     {
-        public string messege;
-        public string word;
-        public int parse_symbol { get; set; }
 
         public conversion(string messege)
         {
-            //List<int> mes = new List<int>();
+            string Bitmessage = "";
+            List<List<int>> Bitmask = new List<List<int>>();
             for (int i = 0; i < messege.Length; i++)
             {
-                word += Convert.ToString(messege[i], 2);
-            }
-            //MessageBox.Show(word.ToString());
-        }
+                Bitmessage += Convert.ToString(messege[i], 2);
+                Bitmessage = String.Concat('0', Bitmessage);
 
-        public List<int> Transfer()
-        {            
-            MessageBox.Show(word);
-            List<int> parsed = new List<int>();
-            int word_number = 0, four_symbols_int;
-            while (word.Length != 0)
-            {
-                string four_symbols = String.Empty;
-                
-                for(int i = 0; i < 4; i++)
+                while (Bitmessage.Length < 1)
                 {
-                    if (String.IsNullOrEmpty(word)) { break; }
-                    int first_element = 0;
-                    char symbol = word[first_element];
-                    //MessageBox.Show(symbol.ToString());
-                    string test = symbol.ToString();
-                    four_symbols = four_symbols + test ;
-                    word = word.Remove(0,1);
-                    //char test = four_symbols[i];
-                    //MessageBox.Show( test .ToString());
-                    //four_symbols.PadRight(i, symbol); // в чаре хрен пойми какой символ. нужно преобразовать в строку и тестить
-                    //four_symbols.PadLeft(i, symbol);
+                    List<int> bit4 = new List<int>();
 
+                    for (int j = 0; j < 4; j++)
+                    {
+                        bit4.Add((int)Char.GetNumericValue(Bitmessage[j]));
+                    }
+                    MessageBox.Show(Bitmessage);
+                    MessageBox.Show(Bitmessage);
+
+                    Bitmessage = "";
                 }
-                four_symbols_int = Convert.ToInt32(four_symbols);
-                parsed.Add(word_number);
-                parsed.Insert(word_number, four_symbols_int);
-                //MessageBox.Show((word_number, "номер слова", four_symbols, "символы в строке", four_symbols_int, "символы в INT").ToString());
-                word_number++;
             }
-            MessageBox.Show(parsed.ToString());
-            return parsed;
+            //Transfer(Bitmessage);
         }
+
+       /* public List<List<int>> Transfer(string Bitmessage)
+        {            
+
+            //return parsed;
+        }*/
     }
     
 }
