b3078418bffa3a2c74769d39f14c4691ce534e50
Unknown <Kizary-archer@yandex.ru>
2019-06-18 21:48:21 +0300

    RS_multi

---
+++ Hamming/RS_multi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Hamming
+{
+    static class RS_multi
+    {
+      private static List<int> alfam = new List<int>() { 1, 2, 4, 8, 3, 6, 12, 11, 5, 10, 7, 14, 15, 13, 9, 1 };
+
+        public static int alfa(int a ,int b)
+        {
+            int res = alfam[a+b];
+            return res;
+        }
+    }
+}
